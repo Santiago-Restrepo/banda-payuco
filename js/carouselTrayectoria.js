10549e4849fcb40a735aea8404bf7d15a2f13df0
Santiago Restrepo <santirstrp@gmail.com>
2020-11-19 22:28:05 -0500

    Etapa de pruebas con carouseles

--- js/carouselTrayectoria.js
+++ js/carouselTrayectoria.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const elementosCarousel= document.querySelectorAll('.carousel');
     M.Carousel.init(elementosCarousel, {
-        duration: 1500
+        duration: 1500,
+        shift: 5
         
     });
 })
